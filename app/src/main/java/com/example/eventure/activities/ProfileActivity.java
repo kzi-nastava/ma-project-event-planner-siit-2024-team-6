aa719602ebfba750c3a844cbaea5c704896a5807
Maksim Privalov <93774216+maksimprivalov@users.noreply.github.com>
2024-11-11 13:36:06 +0100

    Added ProfileActivity

---
+++ app/src/main/java/com/example/eventure/activities/ProfileActivity.java
@@ -0,0 +1,38 @@
+package com.example.eventure.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eventure.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        Button btnRegister = findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
