7bd20775df8dab76e157c994eea63e8ca8218a39
maksimprivalov <mprivalovs@gmail.com>
2024-11-11 16:41:27 +0100

    [ADD] Bottom view

--- app/src/main/java/com/example/eventure/activities/ProfileActivity.java
+++ app/src/main/java/com/example/eventure/activities/ProfileActivity.java
@@ -7,9 +7,13 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import com.example.eventure.R;
+import com.example.eventure.fragments.EventsFragment;
+import com.example.eventure.fragments.PasFragment;
 import com.example.eventure.fragments.ProfileStartFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -25,11 +29,32 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("EVENTURE");
         toolbar.setTitleTextColor(getResources().getColor(R.color.white));
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new ProfileStartFragment())
                     .commit();
         }
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.events_menu) {
+            selectedFragment = new EventsFragment();
+        } else if (itemId == R.id.pas_menu) {
+            selectedFragment = new PasFragment();
+        }
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
+
 }
 
