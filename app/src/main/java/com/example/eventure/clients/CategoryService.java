b50a131b3d0e0484ffa8b981574ce9bacc1089e2
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-15 02:41:18 +0100

    [ADD] a service to manage categories

---
+++ app/src/main/java/com/example/eventure/clients/CategoryService.java
@@ -0,0 +1,19 @@
+package com.example.eventure.clients;
+
+import com.example.eventure.model.Category;
+import com.example.eventure.model.Offer;
+import com.example.eventure.model.PagedResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface CategoryService {
+    @GET("providers/categories")
+    Call<List<Category>> getAllCategories();
+
+
+}
