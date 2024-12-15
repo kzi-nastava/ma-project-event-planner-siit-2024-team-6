5ef7dd6c2f4cdc2dfdcbca0ce2005a50895b5bec
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2024-12-15 02:42:19 +0100

    [ADD] getters and setters

--- app/src/main/java/com/example/eventure/dto/EventTypeDTO.java
+++ app/src/main/java/com/example/eventure/dto/EventTypeDTO.java
@@ -13,4 +13,22 @@ public class EventTypeDTO {
         this.name = e.getName();
         this.isDeleted = e.getIsDeleted();
     }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getDescription(){
+        return  description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public boolean getIsDeleted(){
+        return isDeleted;
+    }
+    public void setIsDeleted(boolean value){
+        isDeleted = value;
+    }
 }
