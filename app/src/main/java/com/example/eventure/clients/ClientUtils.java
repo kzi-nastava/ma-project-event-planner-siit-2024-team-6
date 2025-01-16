9d2476c4bc7fe65a353ebf5a5135972ab7826a1a
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-01-17 00:27:15 +0100

    [ADD] event service and curstom gson for localdatetime deserializing

--- app/src/main/java/com/example/eventure/clients/ClientUtils.java
+++ app/src/main/java/com/example/eventure/clients/ClientUtils.java
@@ -1,7 +1,11 @@
 package com.example.eventure.clients;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -9,27 +13,37 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import com.example.eventure.BuildConfig;
 
 public class ClientUtils {
-    public static final String SERVICE_API_PATH = "http://"+BuildConfig.IP_ADDR+":8080/api/"; // For Android emulator
-    public static OkHttpClient test(){
+    public static final String SERVICE_API_PATH = "http://" + BuildConfig.IP_ADDR + ":8080/api/"; // For Android emulator
+    public static final String LOGIN = "users/login";
+    public static final String TOP_FIVE = "events/top-five";
+    public static final String ALL_EVENTS = "events/";
+
+    // Create a custom Gson instance with LocalDateTime deserializer
+    private static Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, typeOfT, context) ->
+                    LocalDateTime.parse(json.getAsString()))
+            .create();
+
+    public static OkHttpClient test() {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client = new OkHttpClient.Builder()
+        return new OkHttpClient.Builder()
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
                 .writeTimeout(120, TimeUnit.SECONDS)
-                .addInterceptor(interceptor).build();
-
-        return client;
+                .addInterceptor(interceptor)
+                .build();
     }
 
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(SERVICE_API_PATH)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .client(test())
             .build();
 
     public static final EventTypeService eventTypeService = retrofit.create(EventTypeService.class);
+    public static final EventService eventService = retrofit.create(EventService.class);
     public static final OfferService offerService = retrofit.create(OfferService.class);
     public static final CategoryService categoryService = retrofit.create(CategoryService.class);
 }
---
+++ app/src/main/java/com/example/eventure/clients/EventService.java
@@ -0,0 +1,17 @@
+package com.example.eventure.clients;
+
+import com.example.eventure.dto.EventDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EventService {
+    @GET(ClientUtils.TOP_FIVE)
+    Call<List<EventDTO>> getTopFive();
+
+    @GET(ClientUtils.ALL_EVENTS)
+    Call<List<EventDTO>> getAll();
+
+}
