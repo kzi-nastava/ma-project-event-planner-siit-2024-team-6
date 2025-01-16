3d0be170b002170145bea16164ec4a2a41b438ae
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-01-17 00:24:36 +0100

    [ADD] glide for displaying photos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,6 @@ dependencies {
     implementation("androidx.paging:paging-runtime:2.1.2")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
 }
