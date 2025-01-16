20a9358e01fe9f468fd6d284a197a724b28edc7c
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-01-17 00:27:38 +0100

    [ADD] event dto

---
+++ app/src/main/java/com/example/eventure/dto/EventDTO.java
@@ -0,0 +1,86 @@
+package com.example.eventure.dto;
+
+import com.example.eventure.model.Event;
+import com.example.eventure.model.Status;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class EventDTO {
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer maxParticipants;
+    private Integer participants;
+    private Boolean isPublic;
+    private String place;
+    private LocalDateTime date;
+    private EventTypeDTO eventType;
+    private List<String> photos;
+    private Boolean isDeleted;
+    private Double rating;
+
+    public EventDTO(Integer id, String name, String description, Integer maxParticipants, Integer participants, Boolean isPublic, String place, LocalDateTime date, EventTypeDTO eventType, List<String> photos, Boolean isDeleted, Double rating) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.maxParticipants = maxParticipants;
+        this.participants = participants;
+        this.isPublic = isPublic;
+        this.place = place;
+        this.date = date;
+        this.eventType = eventType;
+        this.photos = photos;
+        this.isDeleted = isDeleted;
+        this.rating = rating;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getMaxParticipants() {
+        return maxParticipants;
+    }
+
+    public Integer getParticipants() {
+        return participants;
+    }
+
+    public Boolean getPublic() {
+        return isPublic;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public EventTypeDTO getEventType() {
+        return eventType;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+}
