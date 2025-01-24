b52016ba4f948aa2a3070ce41247249a7adc7b15
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-01-24 15:38:26 +0100

    [ADD] login dtos

---
+++ app/src/main/java/com/example/eventure/dto/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.eventure.dto;
+
+public class LoginResponseDTO {
+    private String token;
+
+    // Getter and Setter for token
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
