cc2cd95bbf2f934521544b29b7db413f0d9a24df
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-02 22:25:40 +0200

    [ADD] classes for provider's company management

---
+++ app/src/main/java/com/example/eventure/dto/ProviderDTO.java
@@ -0,0 +1,40 @@
+package com.example.eventure.dto;
+
+public class ProviderDTO {
+    private int id;
+    private String companyEmail;
+    private String companyName;
+    private String companyAddress;
+    private String description;
+    private String[] companyPhotos;
+    private String openingTime;
+    private String closingTime;
+
+    public String getClosingTime() {
+        return closingTime;
+    }
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getOpeningTime() {
+        return openingTime;
+    }
+
+    public String[] getCompanyPhotos() {
+        return companyPhotos;
+    }
+
+    public int getId() {return id;}
+}
