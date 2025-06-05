cab0c802cce979663f107ac824e0185b4e8d43f7
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-05 22:36:07 +0200

    [ADD] data transfer objects for reaction entity

---
+++ app/src/main/java/com/example/eventure/dto/ReactionDTO.java
@@ -0,0 +1,10 @@
+package com.example.eventure.dto;
+
+public class ReactionDTO {
+    private Integer id;
+    private String text;
+    private Integer rating;
+    private String status;
+
+    public ReactionDTO(){}
+}
