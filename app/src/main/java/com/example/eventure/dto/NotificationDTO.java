351308f00708336a2a4f79220a95c2579456878d
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-06-09 19:20:58 +0200

    [ADD] notification dtos

---
+++ app/src/main/java/com/example/eventure/dto/NotificationDTO.java
@@ -0,0 +1,42 @@
+package com.example.eventure.dto;
+
+import java.time.LocalDateTime;
+
+public class NotificationDTO {
+    private Integer id;
+    private String text;
+    private Integer receiverId;
+    private LocalDateTime timestamp;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Integer receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
