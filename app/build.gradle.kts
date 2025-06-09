ccc1e503bbea859c4308802afcfd7b47ee223550
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-06-09 19:19:46 +0200

    [ADD] dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation(libs.recyclerview)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:3.12.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.code.gson:gson:2.8.8")
     implementation("com.github.bumptech.glide:glide:4.15.1")
     implementation("androidx.paging:paging-runtime:2.1.2")
