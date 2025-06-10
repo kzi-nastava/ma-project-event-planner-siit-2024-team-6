31d9b750b65733ef4552d399c01f9a89353e921f
Darinka Loncar <149894872+darinkaloncar@users.noreply.github.com>
2025-06-10 22:05:56 +0200

    [ADD] call to endpoint to get chat

--- app/src/main/java/com/example/eventure/clients/ChatService.java
+++ app/src/main/java/com/example/eventure/clients/ChatService.java
@@ -1,13 +1,17 @@
 package com.example.eventure.clients;
 
+import com.example.eventure.dto.ChatWithMessagesDTO;
 import com.example.eventure.model.Chat;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface ChatService {
     @GET("chats/")  // replace with full path if necessary
     Call<List<Chat>> getAllChats();
+    @GET("chats/{chatId}")
+    Call<ChatWithMessagesDTO> getChat(@Path("chatId") int chatId);
 }
