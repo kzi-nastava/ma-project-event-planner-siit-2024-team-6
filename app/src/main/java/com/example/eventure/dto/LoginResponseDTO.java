0c7ff7dc9da0d9639f763840eae1a0276250dc41
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2025-06-13 23:59:15 +0200

    [ADD] management of muted field for logged-in user

--- app/src/main/java/com/example/eventure/dto/LoginResponseDTO.java
+++ app/src/main/java/com/example/eventure/dto/LoginResponseDTO.java
@@ -2,6 +2,7 @@ package com.example.eventure.dto;
 
 public class LoginResponseDTO {
     private String token;
+    private boolean muted;
 
     // Getter and Setter for token
     public String getToken() {
@@ -11,4 +12,13 @@ public class LoginResponseDTO {
     public void setToken(String token) {
         this.token = token;
     }
+
+    public boolean isMuted() {
+        return muted;
+    }
+
+    public void setMuted(boolean muted) {
+        this.muted = muted;
+    }
+
 }
